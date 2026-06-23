8e995c097587a2bc9be2f78b64818d6eae3dc882
Juan <centrolibrepiensa@gmail.com>
2026-06-22 23:11:09 -0400

    fix: jest setupFilesAfterEnv

--- jest.config.ts
+++ jest.config.ts
@@ -6,7 +6,7 @@ const createJestConfig = nextJest({ dir: './' })
 const config: Config = {
   coverageProvider: 'v8',
   testEnvironment: 'jsdom',
-  setupFilesAfterFrameworkFileFile: ['<rootDir>/jest.setup.ts'],
+  setupFilesAfterEnvFile: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
