5ed710e6a39dca282c15a39eb73f03409a37d9d0
Juan <centrolibrepiensa@gmail.com>
2026-06-22 23:15:12 -0400

    fix: jest config correcto

--- jest.config.ts
+++ jest.config.ts
@@ -6,11 +6,11 @@ const createJestConfig = nextJest({ dir: './' })
 const config: Config = {
   coverageProvider: 'v8',
   testEnvironment: 'jsdom',
-  setupFilesAfterEnvFile: ['<rootDir>/jest.setup.ts'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
-  testPathPattern: ['**/__tests__/**/*.test.ts?(x)'],
+  testMatch: ['**/__tests__/**/*.test.ts?(x)'],
 }
 
 export default createJestConfig(config)
